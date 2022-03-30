2476621c932c8d09f95560b0df1069dcad4e5691
rezek <rezek@H3CT1C-PC>
2022-03-30 21:16:05 +0200

    Add project files.

---
+++ Übungen/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Gib mal bitte die erste Zahl: ");
+string input1Zahl = Console.ReadLine();
+
+Console.Write("Und nun gib bitte die zweite Zahl: ");
+string input2Zahl = Console.ReadLine();
+
+
+int input1ZahlAlsInt = int.Parse(input1Zahl);
+int input2ZahlAlsInt = int.Parse(input2Zahl);
+int ergibis = input1ZahlAlsInt * input2ZahlAlsInt;
+
+
+Console.WriteLine($"{input1Zahl} * {input2Zahl} = {ergibis}");
+
+
+
+
+
+
